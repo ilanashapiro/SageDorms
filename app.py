be237200b96fd5ab99699f173d33dbe0ae4c93a7
Gabe Alzate <ggalzate98@gmail.com>
2020-04-14 03:16:15 -0700

    added cas login capabilities

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sagedorm_db
 import random
+import cas_login
 
 app = Flask(__name__)
+app.secret_key = "shhhhh keep it a secret"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # student selected housing
-    if request.method == 'POST':
+    # # student selected housing
+    # if request.method == 'POST':
 
-        # save all inputted data
-        student = request.form
-        name = student['name'].split(" ") #TODO: name does nothing for us rn
-        studInfo = {}
-        studInfo['fname'] = name[0]
-        studInfo['lname'] = name[1]
-        studInfo['sid'] = int(student['sid'])
-        studInfo['dormName'] = student['dormName']
-        studInfo['dormRoom'] = random.randint(1, 999)
+        # # save all inputted data
+        # student = request.form
+        # name = student['name'].split(" ")
+        # studInfo = {}
+        # studInfo['fname'] = name[0]
+        # studInfo['lname'] = name[1]
+        # studInfo['sid'] = int(student['sid'])
+        # studInfo['dormName'] = student['dormName']
+        # studInfo['dormRoom'] = random.randint(1, 999)
 
-        # initialize mysql server and database
-        sagedorm_db.main('u', studInfo)
-        return redirect('/students')
-
-    return render_template('index.html')
+        # # initialize mysql server and database
+        # sagedorm_db.main('u', studInfo)
+        # return redirect('/students')
+    loggedIn = False
+    if 'cookies' in session and session['cookies']:
+        print(session['cookies'])
+        loggedIn = True
+    return render_template('index.html', loggedIn=loggedIn)
 
 @app.route('/students')
 def students():
@@ -33,6 +38,37 @@ def students():
 @app.route('/dorms')
 def dorms():
     return render_template('generic.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    # submitted login form
+    if request.method == 'POST':
+        info = request.form
+
+        # not used in CAS login form but rather for ourselves
+        user_info = {}
+        user_info['school'] = info['school']
+        user_info['sid'] = info['sid']
+
+        # used in login
+        login_info = {}
+        login_info['username'] = f"{info['dispname']}@{info['school']}.edu"
+        login_info['dispname'] = info['dispname']
+        login_info['password'] = info['password']
+
+        # session is a built in vbl that persists as long as the app is running
+        # login using an external python script. once we login, we save the
+        # cookies of the login throughout the app
+        session['cookies'] = cas_login.main(login_info)
+        return redirect('/')
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
