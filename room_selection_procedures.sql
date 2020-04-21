eede878861c445ae274ca055185f98602fa4cf05
Ilana Shapiro <ilanashapiro@Ilanas-MacBook-Pro.local>
2020-04-21 00:11:49 -0700

    added stored functions for checking if the student has been assigned already to a suite (limits their room selection options) or to a room

--- room_selection_procedures.sql
+++ room_selection_procedures.sql
@@ -54,7 +54,7 @@ CREATE PROCEDURE RemoveMyselfFromSuiteGroup(
 	IN email VARCHAR(26)
 )
 BEGIN
-	UPDATE SuiteGroup AS sg
+	UPDATE SuiteGroup AS sg -- recompute average draw num for all remaining members of group. If the removal happens before the draw, this affects their draw time
 	SET sg.avgDrawNum = SELECT avg(s.drawNum)
 						FROM Student AS s
 						WHERE s.email!= email
@@ -74,7 +74,9 @@ BEGIN
 	WHERE sg.email = email; -- delete the student from the suite group. This can be done anythime (including during suite draw) before their suite draw time is reached
 END $$
 
-CREATE PROCEDURE GetAllDormRoomsSummary()
+CREATE PROCEDURE GetAllDormRoomsSummary(
+	IN email
+)
 BEGIN
 	SELECT r.number, r.squareFeet, r.otherDescription,
 		   dr.numOccupants, dr.connectingRoomNum
@@ -85,8 +87,8 @@ BEGIN
 END $$
 
 CREATE PROCEDURE GetRoomDetails(  -- common or dorm
-	IN roomNum
-	IN dormName
+	IN roomNum INT,
+	IN dormName VARCHAR(50)
 )
 BEGIN
 	SELECT r.number, r.squareFeet, r.otherDescription,
@@ -108,4 +110,45 @@ BEGIN
 	WHERE dr.dormRoomNum = sr.number AND dr.dormName = sr.dormName
 		  AND cr.number = sr.number AND cr.dormName = sr.dormName
 	ORDER BY sr.dormName, sr.suiteID, sr.number; -- group first by dorm, alphabetically, then group data by suite for later processing, then finally by room number, for later processing
+END $$
+
+CREATE PROCEDURE GetMySuiteRooms(
+	IN email VARCHAR(26)
+)
+BEGIN
+	SELECT sr.suiteID, sr.isSubFree, sr.numRooms, sr.dormName, sr.otherDescription
+		   sr.number, sr.squareFeet, sr.otherDescription,
+		   dr.numOccupants, dr.connectingRoomNum
+		   cr.hasStove, cr.hasSink, cr.hasRefrigerator, cr.hasBathroom
+	FROM DormRoom AS dr, CommonRoom AS cr, (SELECT * FROM Room AS r LEFT JOIN Suite AS s ON r.suiteID = s.suiteID) AS sr, SuiteGroup AS sg
+	WHERE dr.dormRoomNum = sr.number AND dr.dormName = sr.dormName
+		  AND cr.number = sr.number AND cr.dormName = sr.dormName
+		  AND sg.email = email AND sg.suiteID IS NOT NULL -- select only the rooms in the suite the student is in. If the student doesn't have a suite, nothing gets returned
+	ORDER BY sr.dormName, sr.suiteID, sr.number; -- group first by dorm, alphabetically, then group data by suite for later processing, then finally by room number, for later processing
+END $$
+
+DROP FUNCTION IF EXISTS CheckIfStudentHasRoom$$
+CREATE FUNCTION CheckIfStudentHasRoom(
+	IN email VARCHAR(26)
+)
+RETURNS BOOL
+DETERMINISTIC
+BEGIN
+	RETURN EXISTS (
+		SELECT *
+		FROM Student AS s
+		WHERE s.email = email AND s.roomNum IS NOT NULL);
+END $$
+
+DROP FUNCTION IF EXISTS CheckIfStudentHasSuite$$
+CREATE FUNCTION CheckIfStudentHasSuite(
+	IN email VARCHAR(26)
+)
+RETURNS BOOL
+DETERMINISTIC
+BEGIN
+	RETURN EXISTS (
+		SELECT *
+		FROM SuiteGroup AS sg
+		WHERE sg.email = email AND sg.suiteID IS NOT NULL);
 END $$
--- table_verification_functions.sql
+++ table_verification_functions.sql
@@ -1,7 +1,7 @@
 -- used to check after the draw if all students living on campus (isDrawing = true) have drawn rooms
 DELIMITER $$
-DROP FUNCTION IF EXISTS checkStudentsChooseRooms$$
-CREATE FUNCTION checkStudentsChooseRooms()
+DROP FUNCTION IF EXISTS CheckStudentsChooseRooms$$
+CREATE FUNCTION CheckStudentsChooseRooms()
 RETURNS BOOL
 DETERMINISTIC
 BEGIN
@@ -13,15 +13,15 @@ BEGIN
 			AND s.dormName IS NULL );  -- the dorm of the connecting room must be the same as that of the dorm room
 END $$
 
-DROP FUNCTION IF EXISTS checkSuiteHasRepresentative$$
-CREATE FUNCTION checkSuiteHasRepresentative()
+DROP FUNCTION IF EXISTS CheckSuiteHasRepresentative$$
+CREATE FUNCTION CheckSuiteHasRepresentative()
 RETURNS BOOL
 DETERMINISTIC
 BEGIN
 	RETURN NOT EXISTS (
 		SELECT *
 		FROM
-			(SELECT avgSuiteGroupDrawNum, max(isSuiteRep) AS containsSuiteREP
+			(SELECT avgSuiteGroupDrawNum, max(isSuiteRep) AS containsSuiteRep
 			FROM Student
 			WHERE avgSuiteGroupDrawNum IS NOT NULL
 			GROUP BY avgSuiteGroupDrawNum)
