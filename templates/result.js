87b1969cd3d439f75f9a08a9e2da8fb95fd547b3
Yurie Muramatsu <yuriemuramatsu@gmail.com>
2020-04-29 15:10:01 +0800

    delete works! kinda

--- templates/result.js
+++ templates/result.js
@@ -1,28 +1,55 @@
 window.onload = function () {
+
     var rooms = JSON.parse(localStorage.getItem("rooms")),
-    ele = document.getElementById('js_result_list'),
-    fragment = document.createDocumentFragment();
+        ele = document.getElementById('js_result_list'),
+        fragment = document.createDocumentFragment(),
+        button;
 
-    console.log(rooms)
-  
+    window.onpageshow = function (event) {
+        if (event.persisted) {
+            window.location.reload()
+        }
+    };
+
+    console.log(rooms);
+
     if (rooms) {
-      for (var i = 0; i < rooms.length; i++) {
+        for (var i = 0; i < rooms.length; i++) {
 
-        var li = document.createElement('li'),
-            h2 = document.createElement('h2'),
-            name = document.createElement('div');
-            // dorm = documen.createElement('div');
+            var li = document.createElement('li'),
+                h2 = document.createElement('h2'),
+                name = document.createElement('div'),
+                button = document.createElement("button");
+            button.innerHTML = "Delete";
+            button.style.fontSize = '15px';
 
-        name.classList.add('name');
-        // dorm.classList.add('dorm');
-        
-      
-        h2.appendChild(document.createTextNode(rooms[i].name));
-        // h2.appendChild(document.createTextNode(rooms[i].dorm));
-        li.appendChild(h2);
-        fragment.appendChild(li);
-      }
+            name.classList.add('name');
+            h2.appendChild(document.createTextNode(rooms[i].name));
+            h2.appendChild(document.body.appendChild(button));
+
+            li.appendChild(h2);
+            fragment.appendChild(li);
+
+            h2.setAttribute("id", i);
+        }
     }
-  
-    ele.appendChild(fragment);  
-  };
+
+    ele.appendChild(fragment);
+
+    var all_buttons = document.querySelectorAll('button');
+
+    all_buttons.forEach(function (all_buttons, index) {
+        all_buttons.addEventListener('click', function () {
+
+            document.getElementById(index).remove();
+
+            rooms.splice(index, 1);
+            localStorage.setItem("rooms", JSON.stringify(rooms));
+
+            console.log(rooms);
+
+            //problem: when you delete a room, it appears to be deleted on page but sometimes doesn't actually
+            //remove from array
+        });
+    });
+};
--- templates/wishlist.js
+++ templates/wishlist.js
@@ -1,13 +1,20 @@
 window.onload = function () {
 
-    localStorage.clear();
+    // localStorage.clear();
 
     var wishlist_button = document.querySelectorAll('.js_wishlist_button'),
         clicked = [],
         save_rooms = [],
         rooms = JSON.parse(localStorage.getItem("rooms"));
 
+    window.onpageshow = function (event) {
+        if (event.persisted) {
+            window.location.reload()
+        }
+    };
+
     console.log(rooms)
+
     if (rooms) {
         var id;
         for (var i = 0; i < rooms.length; i++) {
@@ -17,25 +24,14 @@ window.onload = function () {
         }
     }
 
-    wishlist_button.forEach(function (wishlist_button,index) {
-        wishlist_button.addEventListener('click',function () {
+    wishlist_button.forEach(function (wishlist_button, index) {
+        wishlist_button.addEventListener('click', function () {
 
             if (clicked.indexOf(index) >= 0) {
                 console.log("You can't add anymore!")
-                console.log(clicked)
 
-                // for (var i = 0; i < clicked.length; i++) {
+            } else if (clicked.indexOf(index) == -1) { // nothing in clicked list
 
-                //     // get the room we clicked
-                //     if(clicked[i] == index){
-                //         clicked.splice(i, 1);
-                //         save_rooms.splice(i, 1);
-                //         break;
-                //     }
-                // }
-
-                
-            } else if(clicked.indexOf(index) == -1){ // nothing in clicked list
 
                 var name = wishlist_button.dataset.name;
 
@@ -44,12 +40,12 @@ window.onload = function () {
                     id: index,
                     name: name,
                 });
-                console.log(clicked)
-                console.log(save_rooms)
+
+
             }
+            console.log(save_rooms);
 
-            localStorage.setItem("rooms",JSON.stringify(save_rooms));
-
+            localStorage.setItem("rooms", JSON.stringify(save_rooms));
         });
     });
 };  
