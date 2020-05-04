f347bed27a11029e5b1cedff4b7fb68c0eb04f92
Gabe Alzate <ggalzate98@gmail.com>
2020-05-03 20:37:29 -0700

    fixed wishlist 1 thingy

--- app.py
+++ app.py
@@ -14,59 +14,6 @@ app.secret_key = "shhhhh keep it a secret"
 @app.route('/', methods=['GET', 'POST'])
 # called when you go to localhost 5000
 def index():
-    # # student selected housing
-    # if request.method == 'POST':
-        # # save all inputted data
-        # student = request.form
-        # name = student['name'].split(" ")
-        # studInfo = {}
-        # studInfo['fname'] = name[0]
-        # studInfo['lname'] = name[1]
-        # studInfo['sid'] = int(student['sid'])
-        # studInfo['dormName'] = student['dormName']
-        # studInfo['dormRoom'] = random.randint(1, 999)
-
-        # table to be properly created later when parsing room request data (i.e. user-selected filters)
-        # for now, this is psuedocode that will be referenced in the dynamically built SQL query in sagedorm_db.property
-        # info = request.form
-        # roomInfo = {}
-        # roomInfo['dormNum'] = .....
-        # roomInfo['dormName'] = .....
-        # roomInfo['numOccupants'] = .....
-        # roomInfo['hasPrivateBathroom'] = .....
-        # roomInfo['numDoors'] = .....
-        # roomInfo['closetType'] = .....
-        # roomInfo['hasConnectingRoom'] = .....
-        # roomInfo['floorNum'] = .....
-        # roomInfo['squareFeet'] = .....
-        # roomInfo['isSubFree'] = .....
-        # roomInfo['windowType'] = .....
-        # roomInfo['isSuite'] = .....
-        # sagedorm_db.selectRooms(....)
-
-        # table to be properly created later when parsing student chooses room data
-        # is there a way to remember which student is logged in, and get the SID from there without the student inputting it directly into a form???
-        # info = request.form
-        # roomChoiceInfo = {}
-        # roomChoiceInfo['dormRoomNum'] = .....
-        # roomChoiceInfo['dormName'] = .....
-        # roomChoiceInfo['SID'] = somehow get the sid of the logged in student directly???
-
-        # table to be properly created later when parsing create prospective suite group data
-        # Idea is the student creating the group puts all OTHER members' SIDs in the form (So up to 5 for 6 total), and the student filling out the form becomes the suite group
-        #       representative that will register the suite later on
-        # some fields will be null since a suite can have up to 6 people. However an error should get displayed if the user inputs in less than 2 names (for 3 total)
-        # is there a way to remember which student is logged in, and get the SID from there without the student inputting it directly into a form???
-        # info = request.form
-        # SuiteGroupInfo = {}
-        # SuiteGroupInfo['sid2'] = .....
-        # SuiteGroupInfo['sid3'] = ...
-        # SuiteGroupInfo['sid4'] = ...
-        # SuiteGroupInfo['sid5'] = ...
-        # SuiteGroupInfo['sid6'] = ...
-
-        # initialize mysql server and database
-        # sagedorm_db.main('u', studInfo)
     loggedIn = False
     if 'cookies' in session:
         loggedIn = True
@@ -175,9 +122,9 @@ def displayRoomSelectionInfo():
         rawinfo = request.form
 
         # add to wishlist
-        if len(rawinfo) == 1:
+        if 'wishlist_item' in rawinfo:
             # get email if logged in
-                return addToWishListHelper(rawinfo['room'].split())
+            return addToWishListHelper(rawinfo['wishlist_item'].split())
 
         else:
             info = rawinfo.to_dict(flat=False)
@@ -242,19 +189,9 @@ def viewMyRoom():
             info['emailIDSuiteRep'] = global_vars.emailID
             suite_queries.setSuite(info)
 
-        roomData = room_queries.getMyRoomDetails()
-        suiteData = suite_queries.getMySuiteDetails()
-        dataDict = {'roomData' : roomData, 'suiteData' : suiteData}
-        print(dataDict)
-        return render_template('viewMyRoom.html', data = dataDict)
-
-    print("ID", global_vars.emailID)
-    # info = {'suiteID' : 'oxeoqmej', 'emailIDSuiteRep' : 'hpaa2018'}
-    # suite_queries.setSuite(info)
     roomData = room_queries.getMyRoomDetails()
     suiteData = suite_queries.getMySuiteDetails()
     dataDict = {'roomData' : roomData, 'suiteData' : suiteData}
-    print("DATADICT", dataDict)
     return render_template('viewMyRoom.html', data = dataDict)
 
 @app.route('/viewSuiteMembers', methods=['GET', 'POST'])
